dc078dda752517ce6eb56d6b26ba72be47ed6a04
Baltsarak <evgeniy.bk.89@gmail.com>
2023-04-04 20:47:15 +0300

    creating MainViewModel

---
+++ app/src/main/java/com/baltsarak/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.baltsarak.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.baltsarak.shoppinglist.data.ShopListRepositoryImpl
+import com.baltsarak.shoppinglist.domain.DeleteShopItemUseCase
+import com.baltsarak.shoppinglist.domain.EditShopItemUseCase
+import com.baltsarak.shoppinglist.domain.GetShopItemListUseCase
+import com.baltsarak.shoppinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopItemListUseCase = GetShopItemListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopItemListUseCase.getShopItemList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newShopItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newShopItem)
+        getShopList()
+    }
+}
