61af7a70d9bed5916e5afbdb8f683ddd1bf86994
Baltsarak <evgeniy.bk.89@gmail.com>
2023-04-15 12:01:08 +0300

    Creating a screen layout for adding and editing shop item

---
+++ app/src/main/java/com/baltsarak/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.baltsarak.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.baltsarak.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
